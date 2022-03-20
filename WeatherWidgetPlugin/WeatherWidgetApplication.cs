ebcd5230f84ce725c7101f9c27602bdf04475440
Danol <czdanol@gmail.com>
2022-03-20 12:50:50 +0100

    no message

---
+++ WeatherWidgetPlugin/WeatherWidgetApplication.cs
@@ -0,0 +1,15 @@
+namespace Loupedeck.WeatherWidgetPlugin
+{
+	using System;
+
+	public class WeatherWidgetApplication : ClientApplication
+	{
+		public WeatherWidgetApplication() {
+
+		}
+
+		protected override String GetProcessName() => "";
+
+		protected override String GetBundleName() => "";
+	}
+}
---
+++ WeatherWidgetPlugin/WeatherWidgetCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Loupedeck.WeatherWidgetPlugin
+{
+	class WeatherWidgetCommand : PluginDynamicCommand
+	{
+		public WeatherWidgetCommand() {
+			this.DisplayName = "Weather Widget";
+			this.Description = "Shows information about weather in a given area";
+			this.GroupName = "Weather";
+		}
+
+		protected override bool ProcessButtonEvent(string actionParameter, DeviceButtonEvent buttonEvent) {
+			Console.WriteLine($"Button event {actionParameter} - {buttonEvent}");
+			return true;
+		}
+	}
+}
